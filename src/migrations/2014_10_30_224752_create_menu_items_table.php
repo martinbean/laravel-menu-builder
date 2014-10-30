c732c315aa7543648f1b5c848d3b60f61a91083d
Martin Bean <martin@martinbean.co.uk>
2014-10-30 22:48:50 +0000

    Added migrations

---
+++ src/migrations/2014_10_30_224752_create_menu_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenuItemsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('menu_items', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('menu_id')->unsigned();
+			$table->morphs('navigatable');
+			$table->integer('sort_order')->unsigned();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('menu_items');
+	}
+
+}
